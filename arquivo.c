f0b56af71fb61b3cb574f4145ef69fe4c161a2fa
eloysa <elomartins33@gmail.com>
2019-08-21 17:52:35 -0300

    adicionado os dois arquivos c

---
+++ arquivo.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+void main (){
+int idade;
+printf("Digite dua idade");
+scanf("%d", &idade);
+printf("Essa é sua idade: %d", idade);
+
+
+
+return 0;}
---
+++ arquivo2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+int idade;
+printf("Digite sua idade");
+scanf("%d", &idade);
+printf("Essa é sua idade: %d", idade)";
+
+
+
+
+return 0;}
